02ea8226b0d947b3b36f58cf1f676ae66bcb5be0
Jeremy Abel <jabel415@gmail.com>
2017-01-29 16:55:04 -0500

    Completed getCourses, added getSeatsByCrn

--- data.py
+++ data.py
@@ -1,63 +1,79 @@
+import requests
+import re
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 
-# TODO doConnect, rateMyProf, seatsInfo
+
 
 class Data(object):
 
-    # intiate headless browser and navigate to uncc selfservice schedule page
+    ''' initialize headless browser and navigate to uncc selfservice schedule page '''
     def __init__(self):
         self.driver = webdriver.PhantomJS()
         self.driver.get('https://selfservice.uncc.edu/pls/BANPROD/bwckschd.p_disp_dyn_sched')
 
-    def getCourses(self):
+    # TODO
+    ''' connect to postgresql database '''
+    def dbConnect(self):
+        return
+
+    ''' navigate through selfservice site to get course information for every subject for any passed in term (semester) '''
+    def getCourses(self, term='201750'):
+        subjects = {}
+        courseData = []
         self.dropDown = Select(self.driver.find_element_by_id('term_input_id'))
-        # TODO replace with parsed terms
-        self.dropDown.select_by_value('201750') # First Summer 2017, using this as our proof of concept data for now
+        # self.dropDown.select_by_value('201750')
+        self.dropDown.select_by_value(term) # First Summer 2017, using this as our proof of concept data for now
         submit = self.driver.find_element_by_xpath('/html/body/div[3]/form/input[2]').click()
         pageHTML = BeautifulSoup(self.driver.page_source, 'html.parser')
-        subjects = {}
-        # TODO precondition that only looks at letter based option to only select course subjects
-        for option in pageHTML.find_all('option'):
+        for option in pageHTML.find(id="subj_id").find_all('option'):
             subjects[str(option['value'])] = str(option.text)
+        for key, val in sorted(subjects.items()):
+            if str.isalpha(key) and len(key) == 4:
+                subjSelect = Select(self.driver.find_element_by_xpath('//*[@id="subj_id"]'))
+                subjSelect.select_by_value(key) #change to key when in loop
+                self.driver.find_element_by_xpath('/html/body/div[3]/form/input[12]').click()
+                pageHTML = BeautifulSoup(self.driver.page_source, 'html.parser')
+                z=0
+                for course in pageHTML.find_all('th', class_='ddtitle'):
+                    header = ((course.text).split(' - '))
+                    courseData.append([header])
+                for info in pageHTML.find_all('td', class_='dddefault'):
+                    rows = info.find_all('td')
+                    rows = [elem.text for elem in rows]
+                    if len(rows) > 0:
+                        rows = [x.encode('UTF-8') for x in rows]
+                        courseData[z].append(rows)
+                        z+=1
+                self.driver.find_element_by_link_text('Return to Previous').click()
+        # return array of every course holding
+        return courseData
+        ''' courseData[x] = [['Principles of ACCT I', '30011', 'ACCT 2121', '001'],['Class', '8:00 am - 9:30 am', 'MTWRF', 'Friday 116', 'May 22, 2017 - Jun 26, 2017', 'Lecture', 'Shirley Alyce  Hunter (P)']] '''
 
-        #for key, val in subjects.items():
-        #    if str.isalpha(key) and len(key) == 4:
-                #print key, val
-        subjSelect = Select(self.driver.find_element_by_xpath('//*[@id="subj_id"]'))
-        subjSelect.select_by_value('ACCT') #change to key when in loop
-        self.driver.find_element_by_xpath('/html/body/div[3]/form/input[12]').click()
-        pageHTML = BeautifulSoup(self.driver.page_source, 'html.parser')
-        # TODO change from dictionary to array
-        courseData = {}
-        for course in pageHTML.find_all('th', class_='ddtitle'):
-            header = (str(course.text).split(' - '))
-            ''' print header - ['Principles of ACCT I', '30011', 'ACCT 2121', '001'] '''
-            courseData['title'] = header[0]
-            courseData['crn'] = header[1]
-            courseData['course'] = header[2]
-            courseData['section'] = header[3]
+    ''' get seat data per crn '''
+    def getSeatsByCrn(self, crn='40005', term='201770'):
+        seats = []
+        r = requests.get('https://selfservice.uncc.edu/pls/BANPROD/bwckschd.p_disp_detail_sched?term_in=' + term + '&crn_in=' + crn)
+        pageHTML = BeautifulSoup(r.text, 'html.parser')
+        for row in pageHTML.find_all('td', class_='dddefault', text=re.compile(r'[0-9]{0,4}')):
+            seats.append(row.text)
+        return seats
+    '''
+        seats[0] - capacity
+        seats[1] - actual
+        seats[2] - remaining
+        seats[3] - waitlist capacity
+        seats[4] - waitlist actual
+        seats[5] - waitlist remaining
+    '''
 
-        for info in pageHTML.find_all('td', class_='dddefault'):
-            rows = info.find_all('td')
-            rows = [elem.text for elem in rows]
-            if len(rows) > 0:
-                rows = [x.encode('UTF-8') for x in rows]
-                ''' print rows - ['Class', '8:00 am - 9:30 am', 'MTWRF', 'Friday 116', 'May 22, 2017 - Jun 26, 2017', 'Lecture', 'Shirley Alyce  Hunter (P)'] '''
-                courseData['type'] = rows[0]
-                courseData['time'] = rows[1]
-                courseData['days'] = rows[2]
-                courseData['location'] = rows[3]
-                courseData['dateRange'] = rows[4]
-                courseData['scheduleType'] = rows[5]
-                courseData['instructor'] = rows[6]
-
-                # TODO print testing
-        #for key, val in courseData.iteritems():
-        #    print key, val
-        #print pageHTML.prettify()
+    # TODO
+    ''' get data for every professor from ratemyprofessor.com '''
+    def getProfRating(self, prof):
+        return
 
 
 test = Data()
-test.getCourses()
+#test.getCourses()
+test.getSeatsByCrn()
