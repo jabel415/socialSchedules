d78d73b696e3e2db455d408008afa7d6f67ab959
Jeremy Abel <jabel415@gmail.com>
2017-04-17 13:18:15 -0400

    New Data API

--- data.py
+++ data.py
@@ -1,55 +1,187 @@
-import requests
-import re
+import csv
+import requests, re
+import MySQLdb
+import hashlib
+import html5lib
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup
 
 
 
-class Data(object):
+class fetchData(object):
 
     ''' initialize headless browser and navigate to uncc selfservice schedule page '''
     def __init__(self):
         self.driver = webdriver.PhantomJS()
         self.driver.get('https://selfservice.uncc.edu/pls/BANPROD/bwckschd.p_disp_dyn_sched')
 
-    # TODO
-    ''' connect to postgresql database '''
-    def dbConnect(self):
-        return
+    def config_payloag(self, term='201780', subj='ACCT'):
+        data = [
+                ('term_in', term),
+                ('call_proc_in', 'bwckctlg.p_disp_dyn_ctlg'),
+                ('sel_subj', 'dummy'),
+                ('sel_levl', 'dummy'),
+                ('sel_schd', 'dummy'),
+                ('sel_coll', 'dummy'),
+                ('sel_divs', 'dummy'),
+                ('sel_dept', 'dummy'),
+                ('sel_attr', 'dummy'),
+                ('sel_subj', subj),
+                ('sel_crse_strt', ''),
+                ('sel_crse_end', ''),
+                ('sel_title', ''),
+                ('sel_levl', '%'),
+                ('sel_schd', '%'),
+                ('sel_coll', '%'),
+                ('sel_divs', '%'),
+                ('sel_dept', '%'),
+                ('sel_from_cred', ''),
+                ('sel_to_cred', ''),
+                ('sel_attr', '%'),
+                ]
+        return data
+
+    def call_server(self, url, method="get", data=None, timeout=10):
+        if isinstance(timeout, int):
+            try:
+                if method == "get":
+                    r = requests.get(url, timeout=int(timeout))
+                else:
+                    if data != None:
+                        r = requests.post(url, timeout=int(timeout), data=data)
+                    else:
+                        return ("Error", "You must post data.")
+                if r.status_code == 200:
+                    return r.content
+                else:
+                    return ("Error", r.status_code, r.text)
+            except requests.exceptions.Timeout:
+                return ("Error", "Timeout reached")
+        else:
+            return ("Error", "Timeout must be an integer")
+
+    def get_search_page(self, term):
+        self.dropDown = Select(self.driver.find_element_by_id('term_input_id'))
+        self.dropDown.select_by_value(term)
+        submit = self.driver.find_element_by_xpath('/html/body/div[3]/form/input[2]').click()
+        parser = BeautifulSoup(self.driver.page_source, 'html5lib')
+        return parser
+
+    def get_courses_page(self, term, subj):
+        page_html = self.call_server('https://selfservice.uncc.edu/pls/BANPROD/bwckctlg.p_display_courses', method='post', data=self.config_payloag(term=term, subj=subj), timeout=20)
+        #parser = self.parse_html(page_html)
+        parser = BeautifulSoup(page_html, 'html5lib')
+        return parser
+
+    def get_subjects(self, term, subj):
+        subjects = {}
+        subject_html = self.get_courses_page(term, subj)
+        #subject_html = self.call_server('https://selfservice.uncc.edu/pls/BANPROD/bwckctlg.p_display_courses', method='post', data=self.config_payloag(term=term, subj=subj), timeout=20)
+        #subject_html = BeautifulSoup(subject_html, 'html5lib')
+        for option in subject_html.find(id="subj_id").find_all('option'):
+            subjects[str(option['value'])] = str(option.text)
+        return subjects
+
+    def get_courses(self, term, subj):
+        courses = {}
+        #subjects = self.get_subjects(term, subj)
+        #for key, val in sorted(subjects.items()):
+        courses_html = self.get_courses_page(term, subj)
+        for course in courses_html.find_all('td', class_='nttitle'):
+            course_num = course.text.split()[1]
+            course_title = ' '.join(course.text.split()[3:])
+            courses[course_num] = course_title
+        return courses
+
+    def get_sections(self, term, subj):
+        def search(subj, course_num):
+            subjSelect = Select(self.driver.find_element_by_xpath('//*[@id="subj_id"]'))
+            subjSelect.select_by_value(subj)
+            self.driver.find_element_by_xpath('//*[@id="crse_id"]').send_keys(course_num)
+            self.driver.find_element_by_xpath('/html/body/div[3]/form/input[12]').click()
+        available_subj = []
+        sections = {}
+        courses = {}
+        search_page = self.get_search_page(term)
+        for option in search_page.find(id="subj_id").find_all('option'):
+            available_subj.append(str(option['value']))
+        subjects = self.get_subjects(term, subj)
+        for key, val in (subjects.items()):
+            not_available = False
+            if key not in available_subj:
+                not_available = True
+                break
+            if not_available:
+                continue
+
+            courses = self.get_courses(term, key)
+            for k, v in courses.iteritems():
+                search(key, k)
+                parser = BeautifulSoup(self.driver.page_source, 'html5lib')
+
+                no_results = False
+                # Check if no results found page
+                for head in parser.find_all('td', class_='pldefault'):
+                    header = head.text.encode('utf-8')
+                    if header == '\nNo classes were found that meet your search criteria\n\n':
+                        self.driver.find_element_by_xpath('/html/body/div[3]/table[2]/tbody/tr/td/a').click()
+                        no_results = True
+                        break
+                if no_results:
+                    continue
+
+                for section in parser.find_all('th'):
+                    header = section.text.encode('utf-8').split( ' - ')
+                    print header
+                    #course_name = header[0]
+                    #crn = header[1]
+                    # TODO further pasing of sections if found in search
 
     ''' navigate through selfservice site to get course information for every subject for any passed in term (semester) '''
     def getCourses(self, term='201750'):
         subjects = {}
         courseData = []
         self.dropDown = Select(self.driver.find_element_by_id('term_input_id'))
-        # self.dropDown.select_by_value('201750')
-        self.dropDown.select_by_value(term) # First Summer 2017, using this as our proof of concept data for now
+        # Select semester
+        self.dropDown.select_by_value(term)
         submit = self.driver.find_element_by_xpath('/html/body/div[3]/form/input[2]').click()
         pageHTML = BeautifulSoup(self.driver.page_source, 'html.parser')
         for option in pageHTML.find(id="subj_id").find_all('option'):
             subjects[str(option['value'])] = str(option.text)
         for key, val in sorted(subjects.items()):
             if str.isalpha(key) and len(key) == 4:
+
                 subjSelect = Select(self.driver.find_element_by_xpath('//*[@id="subj_id"]'))
-                subjSelect.select_by_value(key) #change to key when in loop
+                subjSelect.select_by_value(key)
                 self.driver.find_element_by_xpath('/html/body/div[3]/form/input[12]').click()
+                source = self.driver.page_source
                 pageHTML = BeautifulSoup(self.driver.page_source, 'html.parser')
                 z=0
-                for course in pageHTML.find_all('th', class_='ddtitle'):
-                    header = ((course.text).split(' - '))
-                    courseData.append([header])
-                for info in pageHTML.find_all('td', class_='dddefault'):
-                    rows = info.find_all('td')
-                    rows = [elem.text for elem in rows]
-                    if len(rows) > 0:
-                        rows = [x.encode('UTF-8') for x in rows]
-                        courseData[z].append(rows)
-                        z+=1
-                self.driver.find_element_by_link_text('Return to Previous').click()
+                a=0
+
+                for course in pageHTML.find_all('th'):
+                    header = ((course.text))#.split(' - '))
+                #    #courseData.append([header])
+
+        #        for info in pageHTML.find_all('td', class_='dddefault'):
+        #            rows = info.find_all('td')
+        #            rows = [elem.text for elem in rows]
+        #            if len(rows) > 0:
+        #                rows = [x.encode('UTF-8') for x in rows]
+        #                courseData[z][a].append(rows)
+        #                z+=1
+        #        self.driver.find_element_by_link_text('Return to Previous').click()
         # return array of every course holding
-        return courseData
+        #return courseData
         ''' courseData[x] = [['Principles of ACCT I', '30011', 'ACCT 2121', '001'],['Class', '8:00 am - 9:30 am', 'MTWRF', 'Friday 116', 'May 22, 2017 - Jun 26, 2017', 'Lecture', 'Shirley Alyce  Hunter (P)']] '''
+
+    def csvOut(self):
+        myfile = open('data.csv', 'wb')
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for x in self.getCourses():
+            wr.writerow(x)
 
     ''' get seat data per crn '''
     def getSeatsByCrn(self, crn='40005', term='201770'):
@@ -73,7 +205,75 @@ class Data(object):
     def getProfRating(self, prof):
         return
 
+class dbConnect(object):
 
-test = Data()
-#test.getCourses()
-test.getSeatsByCrn()
+    def __init__(self):
+        self.db = MySQLdb.connect(host="localhost",
+                                  user="root",
+                                  passwd="8sg0e$yF",
+                                  db="smart_schedules")
+        self.cursor = self.db.cursor()
+
+    def is_user(self, email):
+        val = self.cursor.execute("SELECT COUNT(*) FROM users WHERE email=%s", (email))
+        return bool(val)
+
+    def create_user(self, f_name, l_name, student_id, email, password, is_ninernet, major="", concentration="", yr_num=1, picture="", notes=""):
+        if not self.is_user(email):
+            cpassword = ""
+            pass_md5 = hashlib.md5(password).hexdigest()
+            self.cursor.execute("INSERT INTO users(firstName, lastName, studentID, email, password, cpassword, major, class_year, concentration, picture_url, is_ninernet, notes)" \
+                                "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (f_name, l_name, student_id, email, pass_md5, cpassword, major, yr_num, concentration, picture, is_ninernet, notes))
+            return True
+        else:
+            return False
+
+    def get_user_by_email(self, email):
+        if self.is_user(email):
+            user_info = self.cursor.execute("SELECT * FROM users WHERE email=%s", (email))
+            return user_info
+        else:
+            return False
+
+    #def authenticate(self, email, password):
+    #    if self.is_user(email):
+
+
+    '''
+    def dbConnect(self):
+        control = connection.cursor()
+        courseData = self.getCourses()
+        for x in range(0, len(courseData)):
+            course = {
+                'crn': courseData[x][1],
+                'course_name': courseData[x][0],
+                'subject': courseData[x][2],  # Strip crn from string
+                'course_num': courseData[x][2],  # String crn from string
+                'credits': '',  # TODO get credit data
+                'course_title': courseData[x][2],
+                'start_date': courseData[x][0],
+                'end_date': courseData[x][0],
+                'start_time': '',
+                'end_time': '',
+                'days': '',
+                'instructor_name': '',
+                'location': '',
+                'year': '',
+                'semester': '',
+                'seat_capacity': '',
+                'seat_accounted': '',
+                'seats_remaining': '',
+                'instructors_id': ''
+            }
+        return control
+        '''
+
+
+
+
+
+test = fetchData()
+test.get_sections(term='201780', subj='')
+#test.call_server('https://selfservice.uncc.edu/pls/BANPROD/bwckctlg.p_display_courses', method='post', data=test.config_payloag(term='201780', subj=''), timeout=20)
+
+#test.getSeatsByCrn()
--- testing.py
+++ testing.py
@@ -1,6 +1,124 @@
 import requests
+import urllib
+from bs4 import BeautifulSoup
+
 
 r = requests.get('https://selfservice.uncc.edu/pls/BANPROD/bwckschd.p_disp_dyn_sched')
 payload1 = {'p_calling_proc': 'bwckschd.p_disp_dyn_sched',
            'p_term': '201710'}
-print r
+
+'''term_in=201780&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy
+&sel_subj=ACCT&sel_crse=&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_attr=%25&begin_hh=0&begin_mi=0&
+begin_ap=a&end_hh=0&end_mi=0&end_ap=a'''
+
+term='201780'
+subj = 'ACCT'
+
+'''data = {
+    'term_in': term,
+    'sel_subj': 'dummy',
+    #'sel_day': 'dummy',
+    'sel_schd': 'dummy',
+    #'sel_insm': 'dummy',
+    #'sel_camp': 'dummy',
+    'sel_levl': 'dummy',
+    'sel_coll': 'dummy',
+    'sel_divs': 'dummy',
+    'sel_dept': 'dummy',
+    #'sel_sess': 'dummy',
+    #'sel_instr': 'dummy',
+    #'sel_ptrm': 'dummy',
+    'sel_attr': 'dummy',
+    'sel_subj': subj,
+    'sel_crse_strt': '',
+    'sel_crse_end': '',
+    'sel_title': '',
+    'sel_schd': '%',
+    'sel_levl': '%',
+    'sel_coll': '%',
+    'sel_divs': '%',
+    'sel_dept': '%',
+    #'sel_insm': '%',
+    'sel_from_cred': '',
+    'sel_to_cred': '',
+    #'sel_camp': '%',
+    #'sel_levl': '%',
+    #'sel_ptrm': '%',
+    'sel_attr': '%',
+    #'begin_hh': '0',
+    #'begin_mi': '0',
+    #'begin_ap': 'a',
+    #'end_hh': '0',
+    #'end_mi': '0',
+    #'end_ap': 'a',
+    'call_proc_in': 'bwckctlg.p_disp_dyn_ctlg'
+        }'''
+
+data=[('term_in','201780'),
+('call_proc_in', 'bwckctlg.p_disp_dyn_ctlg'),
+('sel_subj','dummy'),
+('sel_levl','dummy'),
+('sel_schd','dummy'),
+('sel_coll','dummy'),
+('sel_divs','dummy'),
+('sel_dept','dummy'),
+('sel_attr','dummy'),
+('sel_subj', ''),#'ACCT'),
+('sel_crse_strt',''),
+('sel_crse_end',''),
+('sel_title',''),
+('sel_levl','%'),
+('sel_schd','%'),
+('sel_coll','%'),
+('sel_divs','%'),
+('sel_dept','%'),
+('sel_from_cred',''),
+('sel_to_cred',''),
+('sel_attr','%'),
+]
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Host': 'selfservice.uncc.edu',
+    'Origin': 'https://selfservice.uncc.edu',
+    'Referer': 'https://selfservice.uncc.edu/pls/BANPROD/bwckgens.p_proc_term_date'
+}
+
+cookie = {'__cfduid': 'd8901f6820826fc9d17d8c020532f5c4e1480365574',
+          '__unam': 'a49b7f5-158b9c9bab6-5214072d-8',
+          '__utma': '71266260.157634453.1480360556.1488237453.1490754888.6',
+          '__utmz': '71266260.1490754888.6.6.utmcsr=library.uncc.edu|utmccn=(referral)|utmcmd=referral|utmcct=/',
+          'accessibility': 'false',
+          '_ga': 'GA1.2.157634453.1480360556'}
+
+def call_server(url, method="get", data=None, timeout=10):
+    if isinstance(timeout, int): #checks to see whether timeout is an integer
+        try:
+            if method == "get":
+                r = requests.get(url, timeout=int(timeout))
+            else:
+                if data != None:
+                    r = requests.post(url, timeout=int(timeout), data=data)
+                else:
+                    return ("Error", "You must post data.")
+            if r.status_code == 200:
+                pageHTML = BeautifulSoup(r.text, 'html.parser')
+                return pageHTML
+
+            else:
+                return ("Error", r.status_code, r.text)
+        except requests.exceptions.Timeout:
+            return ("Error", "Timeout reached")
+    else:
+        return ("Error", "Timeout must be an integer")
+
+#print call_server('https://selfservice.uncc.edu/pls/BANPROD/bwckctlg.p_display_courses', method='post', data=data, timeout=20)
+
+a = ['asd', 'dawd', 'dawdawdad']
+b = 'asd'
+if b in a:
+    print 'Match'
